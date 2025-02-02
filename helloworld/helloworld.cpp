14acc3bdadc212a4d20004888307e80778d369cb
David Dansereau <dansereaudd@gmail.com>
2025-02-01 19:17:48 -0600

    C++ Files, projects

---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello, World!";
+  int x{5};
+  int y{4};
+  return 0;
+}
---
+++ multipyBy2/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+
+  std::cout << "Enter an Integer: ";
+
+  int user_input;
+  std::cin >> user_input;
+
+  std::cout << "Double that number is: " << user_input * 2 << "\n";
+}
